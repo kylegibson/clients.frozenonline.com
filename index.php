e6c03c930a364e668e6602afa1585ef7f652c62b
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-12 01:47:21 -0400

    javascript assets. template for page

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="cache-control" content="no-cache" />
+<title>FrozenOnline - Client Area</title>
+<link href="/assets/css/style.css" rel="stylesheet" type="text/css" />
+<script src="/assets/js/jquery-1.6.2.min.js" type="text/javascript"> </script>
+</head>
+<body>
+
+<div id="top">
+<div class="inner">
+<div class="logo">
+<img src="/assets/images/logo.png" alt="frozenonline" />
+</div>
+<div class="rightside">
+<div class="menu">
+<ul>
+<li><a href="/">Home</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+<div>
+</div>
+
+</body>
+</html>
