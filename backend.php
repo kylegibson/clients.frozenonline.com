aa285ae1dd17000933a7acfc256f4b93db70ea99
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-12 04:42:48 -0400

    fixed key name

--- backend.php
+++ backend.php
@@ -2,13 +2,15 @@
 $page = "views/login_form.php";
 $system = null;
 $passwd = null;
-if(isset($_SESSION["system"]) && isset($_SESSION["pwd"])) {
-  $system = $_SESSION["system"];
-  $passwd = $_SESSION["passwd"];
+$k_system = "system";
+$k_passwd = "passwd";
+if(isset($_SESSION[$k_system]) && isset($_SESSION[$k_passwd])) {
+  $system = $_SESSION[$k_system];
+  $passwd = $_SESSION[$k_passwd];
 }
-if(isset($_POST["system"]) && isset($_POST["passwd"])) {
-  $system = strtolower(trim($_POST["system"]));
-  $passwd = trim($_POST["passwd"]);
+if(isset($_POST[$k_system]) && isset($_POST[$k_passwd])) {
+  $system = strtolower(trim($_POST[$k_system]));
+  $passwd = trim($_POST[$k_passwd]);
 }
 if($system != null && $passwd != null) {
   require_once("/home/frozen/phpcassa.php");
@@ -19,8 +21,8 @@ if($system != null && $passwd != null) {
     $system_info = $r = $sys->get($system);
     if($r["status"] == "assigned" && $r["subscription"] != ""
         && hash("sha256", ".:$system.$passwd:.") == $r["slogin_www"]) {
-      $_SESSION["system"] = $system;
-      $_SESSION["passwd"] = $passwd;
+      $_SESSION[$k_system] = $system;
+      $_SESSION[$k_passwd] = $passwd;
       unset($error);
       $page = "views/account_summary.php";
     }
