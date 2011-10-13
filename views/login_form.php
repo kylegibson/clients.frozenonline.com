223786971e40592124293193c5f1d97e11b059d6
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-12 23:52:38 -0400

    inner container for box

--- views/login_form.php
+++ views/login_form.php
@@ -8,20 +8,22 @@ if(!isset($page)) {
 <p>Javascript is required in the Client Area</p>
 </noscript>
 <div class="box login">
-  <h2>Login</h2>
-  <? if(isset($error)) { echo "<p class='error'>$error</p>"; } ?>
-  <form id="login" action="/" method="post">
-  <div>
-    <label for="system">System Name</label> 
-    <input type="text" id="system" name="system" class="system"/>
+  <div class="inner">
+    <h2>Login</h2>
+    <? if(isset($error)) { echo "<p class='error'>$error</p>"; } ?>
+    <form id="login" action="/" method="post">
+    <div>
+      <label for="system">System Name</label> 
+      <input type="text" id="system" name="system" class="system"/>
+    </div>
+    <div>
+      <label for="passwd">Password</label> 
+      <input type="password" id="passwd" name="passwd" class="passwd"/>
+    </div>
+    <div>
+      <label class="result"><span>&nbsp;</span></label>
+      <input type="submit" value="Login" class='submit'/>
+    </div>
+    </form>
   </div>
-  <div>
-    <label for="passwd">Password</label> 
-    <input type="password" id="passwd" name="passwd" class="passwd"/>
-  </div>
-  <div>
-    <label class="result"><span>&nbsp;</span></label>
-    <input type="submit" value="Login" class='submit'/>
-  </div>
-  </form>
 </div> <!-- login -->
