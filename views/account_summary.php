1c797bd44168422fb993f4d8025cfc7f94187dc3
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-13 00:17:09 -0400

    show status, expiration and time remaining

--- views/account_summary.php
+++ views/account_summary.php
@@ -12,15 +12,36 @@ date_default_timezone_set("UTC");
 $expiration = date("Y-m-d H:i e", $expire_sec);
 $expire_delta = $expire_sec - time();
 
+$status = "";
+$time_remaining = "";
+if ($expire_delta > 0) {
+  $status .= "active";
+  $days = intval($expire_delta / 86400);
+  $hours = intval(($expire_delta % 86400) / 3600);
+  if($days > 0) 
+    $time_remaining .= "$days days, ";
+  if($hours > 1) 
+    $time_remaining .= "$hours hours";
+  elseif($hours == 1) 
+    $time_remaining .= "less than 2 hours";
+  else 
+    $time_remaining .= "less than 1 hour";
+} else {
+  $time_remaining = "no time left";
+  $status .= "expired - please renew!";
+}
+
 $card = array(
   "System" => $system,
+  "Status" => $status,
   "Memory" => $system_info["memory"]."MB",
   "CPU Cores" => $system_info["cpu_cores"],
-  "Banwidth" => $system_info["xfer"]."GB",
+  "Bandwidth" => $system_info["xfer"]."GB",
   "VNC Host" => "vnc.".$system_info["provider"],
   "VNC Port" => $system_info["vncport_external"],
   "Contact email" => $sub_info["email"],
   "Expiration" => $expiration,
+  "Time Remaining" => $time_remaining,
 );
 ?>
 <div class="box summary">
@@ -31,3 +52,4 @@ $card = array(
     <? endforeach;?>
   </div>
 </div>
+
