78903cd49b2a798f26b178a9ea8a63ce1ddc6b06
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-13 19:05:28 -0400

    paypal subscription + is_being_reset

--- views/account_summary.php
+++ views/account_summary.php
@@ -6,12 +6,17 @@ if(!(isset($page) && isset($_SESSION["system"]) && isset($_SESSION["passwd"]))) 
 $system = $_SESSION["system"];
 $passwd = $_SESSION["passwd"];
 
-$expire_sec = parse_expire($sub_info["expire"]);
-$expiration = format_expire($expire_sec);
-$expire_delta = $expire_sec - time();
+if(substr($sub, 0, 2) == "S-") {
+  $expiration = "Paypal Subscription";
+  $time_remaining = "N/A";
+} else {
+  $expire_sec = parse_expire($sub_info["expire"]);
+  $expiration = format_expire($expire_sec);
+  $expire_delta = $expire_sec - time();
+  $time_remaining = expire_time_remaining($expire_delta);
+}
 
 $status = $expire_delta > 0 ? "active" : "expired - please renew!";
-$time_remaining = expire_time_remaining($expire_delta);
 
 $card = array(
   "System" => $system,
@@ -25,13 +30,18 @@ $card = array(
   "Expiration" => $expiration,
   "Time Remaining" => $time_remaining,
 );
+$is_being_reset = isset($system_info["control"]) && $system_info["control"] == "reset";
 ?>
 <script src="/assets/js/account_summary.js" type="text/javascript"> </script>
 <div class="box summary">
   <div class="inner">
     <h2>Account Summary</h2>
     <div class="reboot right">
+<?if($is_being_reset):?>
+      <div class="in-progress">Reboot is in progress</div>
+<?else:?>
       <input type="button" value="Reboot"/>
+<?endif?>
       <div class="hidden in-progress">Submitting reboot request</div>
       <div class="hidden are-you-sure">
         <div>Are you sure you want to reboot?</div> 
