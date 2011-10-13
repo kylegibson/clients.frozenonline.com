ad5f70b1c687736bcd0509444f5e03127cba5f5b
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-13 18:18:06 -0400

    moved code into helpers and config

--- backend.php
+++ backend.php
@@ -1,38 +1,20 @@
 <?
+define('ROOT', $_SERVER["DOCUMENT_ROOT"]);
+require_once(ROOT."/config.php");
+
 $request = basename($_SERVER["REQUEST_URI"]);
-$reboot_url = "/f/reboot";
 
 if($request == "logout") {
-  $_SESSION = array();
-  if (ini_get("session.use_cookies")) {
-      $params = session_get_cookie_params();
-      setcookie(session_name(), '', time() - 42000,
-          $params["path"], $params["domain"],
-          $params["secure"], $params["httponly"]
-      );
-  }
-  session_destroy();
-  header("Location: /");
+  require_helper("logout");
+  logout();
   exit;
 }
 
-$pages = array(
-"login" => "views/login_form.php",
-"summary" => "views/account_summary.php",
-);
-
-$titles = array(
-"login" => "Login",
-"summary" => "Account Summary",
-);
-
 $page = $pages["login"];
 $title = $titles["login"];
-$header = "views/header.php";
 
 $system = $passwd = null;
-$k_system = "system";
-$k_passwd = "passwd";
+
 if(isset($_SESSION[$k_system]) && isset($_SESSION[$k_passwd])) {
   $system = $_SESSION[$k_system];
   $passwd = $_SESSION[$k_passwd];
@@ -41,8 +23,7 @@ if(isset($_POST[$k_system]) && isset($_POST[$k_passwd])) {
   $system = strtolower(trim($_POST[$k_system]));
   $passwd = trim($_POST[$k_passwd]);
 }
-$logged_in = false;
-$menu = array("Home:/");
+
 if($system != null && $passwd != null) {
   require_once("/home/frozen/phpcassa.php");
   $error = "Login failed. User does not exist or incorrect password.";
@@ -79,5 +60,6 @@ if($logged_in) {
   $menu[] = "Logout:/f/logout";
   $page = isset($pages[$request]) ? $pages[$request] : $pages["summary"];
   $title = isset($titles[$request]) ? $titles[$request] : $titles["summary"];
+  require_once(ROOT."/helpers/parse_expire.php");
 }
 ?>
