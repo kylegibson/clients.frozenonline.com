fbae887f07c155e0a6cd90266fec11a075e8a376
Kyle Gibson <kyle.gibson@frozenonline.com>
2011-10-12 23:34:48 -0400

    display system info

--- views/account_summary.php
+++ views/account_summary.php
@@ -5,8 +5,16 @@ if(!(isset($page) && isset($_SESSION["system"]) && isset($_SESSION["passwd"]))) 
 }
 $system = $_SESSION["system"];
 $passwd = $_SESSION["passwd"];
+$card = array(
+  "System" => $system,
+  "Memory" => $systems_info["memory"],
+  "CPU Cores" => $system_info["cpu_cores"],
+  "VNC Port" => $system_info["vncport_external"],
+);
 ?>
 <div class="box summary">
   <h2>Account Summary</h2>
-  <p>System: <?=$system?></p>
+  <? foreach($card as $dt => $dd): ?>
+  <dl><dt><?=$dt?></dt><dd><?=$dd?></dd></dl>
+  <? endforeach;?>
 </div>
